027730a258c9797a2d29d065a2ae552a15a954a2
ethanchen7 <ethan.chen7@gmail.com>
2022-04-11 16:54:47 -0700

    sidebar

--- routes/index.js
+++ routes/index.js
@@ -3,25 +3,36 @@ var router = express.Router();
 
 const db = require("../db/models");
 const { requireAuth } = require("../auth");
-const { csrfProtection } = require("./utils");
+const { csrfProtection, asyncHandler } = require("./utils");
 
 /* GET splash page. */
-router.get("/", requireAuth, csrfProtection, function (req, res, next) {
-  console.log(req.session.auth);
-  if (!req.session.auth) {
-    const user = db.User.build();
-    // change to index after
-    res.render("user-register", {
-      user,
-      csrfToken: req.csrfToken(),
-    });
-  } else {
-    res.render("feed");
-  }
-});
+router.get(
+  "/",
+  requireAuth,
+  csrfProtection,
+  asyncHandler(async (req, res, next) => {
+    console.log(req.session.auth);
+    if (!req.session.auth) {
+      const user = db.User.build();
+      const users = await db.User.findAll();
+      console.log(users);
+      // change to index after
+      res.render("user-register", {
+        user,
+        csrfToken: req.csrfToken(),
+      });
+    } else {
+      res.render("feed");
+    }
+  })
+);
 
 router.get("/login", csrfProtection, function (req, res, next) {
   res.render("user-login", { csrfToken: req.csrfToken() });
+});
+
+router.get("/sidebar", (req, res) => {
+  res.render("feed-sidebar");
 });
 
 module.exports = router;
