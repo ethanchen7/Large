21147203151b330950463d97788c37f4f95a0590
ethanchen7 <ethan.chen7@gmail.com>
2022-04-11 13:46:34 -0700

    user seeder data

---
+++ db/seeders/20220411204337-UserData.js
@@ -0,0 +1,48 @@
+"use strict";
+const { faker } = require("@faker-js/faker");
+const bcrypt = require("bcryptjs");
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+    return queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          firstName: faker.name.firstName(),
+          lastName: faker.name.lastName(),
+          userName: faker.internet.userName(),
+          email: faker.internet.email(),
+          hashedPassword: bcrypt.hashSync(faker.internet.password()),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          firstName: faker.name.firstName(),
+          lastName: faker.name.lastName(),
+          userName: faker.internet.userName(),
+          email: faker.internet.email(),
+          hashedPassword: bcrypt.hashSync(faker.internet.password()),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      { returning: true }
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+    return queryInterface.bulkDelete("People", null, {});
+  },
+};
