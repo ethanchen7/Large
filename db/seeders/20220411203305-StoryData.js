95afdc7eff97195a64c7d9852b7fa64491bd64bf
Jonathon Tufts <86862338+jonathontufts@users.noreply.github.com>
2022-04-11 14:37:40 -0600

    sample stories

---
+++ db/seeders/20220411203305-StoryData.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+   return queryInterface.bulkInsert('Stories', [
+     {
+     title: 'lorem ipsum first article',
+     article: 'very short very concise article',
+     tagId: 1,
+     userId: 1,
+     state: 'published'
+   },
+     {
+       title: 'lorem ipsum second article',
+       article: 'very short very concise article about lorem ipsum part 2 ',
+       tagId: 4,
+       userId: 1,
+       state: 'published'
+     },
+     {
+       title: 'lorem ipsum third article',
+       article: 'i am writing an article about the tag id of 7.. very informative stuff',
+       tagId: 7,
+       userId: 1,
+       state: 'published'
+     },
+  ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+   return queryInterface.bulkDelete('People', null, {});
+  }
+};
