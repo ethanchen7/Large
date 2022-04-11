c4ac70adf07c0f585768e8a483e094d0e619e666
Jonathon Tufts <86862338+jonathontufts@users.noreply.github.com>
2022-04-11 14:44:41 -0600

    story seed data

--- db/seeders/20220411203305-StoryData.js
+++ db/seeders/20220411203305-StoryData.js
@@ -14,22 +14,28 @@ module.exports = {
      article: 'very short very concise article',
      tagId: 1,
      userId: 1,
-     state: 'published'
-   },
-     {
-       title: 'lorem ipsum second article',
-       article: 'very short very concise article about lorem ipsum part 2 ',
-       tagId: 4,
-       userId: 1,
-       state: 'published'
-     },
-     {
-       title: 'lorem ipsum third article',
-       article: 'i am writing an article about the tag id of 7.. very informative stuff',
-       tagId: 7,
-       userId: 1,
-       state: 'published'
-     },
+     state: 'published',
+     createdAt: new Date(),
+     updatedAt: new Date()
+    },
+    {
+      title: 'lorem ipsum second article',
+      article: 'very short very concise article about lorem ipsum part 2 ',
+      tagId: 4,
+      userId: 1,
+      state: 'published',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      title: 'lorem ipsum third article',
+      article: 'i am writing an article about the tag id of 7.. very informative stuff',
+      tagId: 7,
+      userId: 1,
+      state: 'published',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
   ], {});
   },
 
