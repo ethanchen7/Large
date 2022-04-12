50ac92605205e5647d497fc185135282161821e7
Agustin Zucca <zucca.agustin98@gmail.com>
2022-04-12 17:18:46 -0500

    Create user logs in automatically

--- routes/users.js
+++ routes/users.js
@@ -21,12 +21,10 @@ router.post(
       userName,
       email,
     });
-    console.log("before", user);
     const validationErrors = validationResult(req);
     if (validationErrors.isEmpty()) {
       const hashedPassword = await bcrypt.hash(password, 10);
       user.hashedPassword = hashedPassword;
-      console.log("after", user);
       await user.save();
       loginUser(req, res, user);
       res.redirect("/");
@@ -34,17 +32,16 @@ router.post(
       const createErrors = validationErrors.array().map((error) => error.msg);
 
       const queries = await splashPageQueries();
-      const { user, stories, tags } = queries;
+      const { stories, tags } = queries;
 
       res.render("user-register", {
-        title: "Register",
         user,
         trending: stories,
         stories,
         tags,
         createErrors,
         csrfToken: req.csrfToken(),
-        errStatusCreate: true
+        errStatusCreate: true,
       });
     }
   })
@@ -55,33 +52,44 @@ router.post(
   csrfProtection,
   loginValidators,
   asyncHandler(async (req, res) => {
+    const queries = await splashPageQueries();
+    const { user, stories, tags } = queries;
     const { email, password } = req.body;
     let logInErrors = [];
     const validatorErrors = validationResult(req);
-
     if (validatorErrors.isEmpty()) {
       const user = await db.User.findOne({
         where: { email },
       });
+      console.log(user);
       if (user !== null) {
         const passwordMatch = await bcrypt.compare(
           password,
           user.hashedPassword.toString()
         );
+        console.log(passwordMatch);
         if (passwordMatch) {
           loginUser(req, res, user);
           return res.redirect("/");
         }
+        // console.log('password check')
       }
       logInErrors.push(
         "Login failed for the provided email address and password."
       );
+      res.render("user-register", {
+        email,
+        logInErrors,
+        user,
+        stories,
+        tags,
+        trending: stories,
+        csrfToken: req.csrfToken(),
+        errStatusLog: true,
+      });
     } else {
       logInErrors = validatorErrors.array().map((error) => error.msg);
 
-      const queries = await splashPageQueries();
-
-      const { user, stories, tags } = queries;
       res.render("user-register", {
         email,
         logInErrors,
