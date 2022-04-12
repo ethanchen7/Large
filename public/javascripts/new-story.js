57cf73a327d3a68672ad0f15d58627fe85c4a493
ethanchen7 <ethan.chen7@gmail.com>
2022-04-12 15:39:02 -0700

    modal configuration

--- public/javascripts/new-story.js
+++ public/javascripts/new-story.js
@@ -1,7 +1,25 @@
 const publishBtn = document.querySelector(".publish-button");
+const publishModal = document.querySelector(".publish-modal");
 const formSubmitBtn = document.querySelector(".submit-btn");
+const storyCreateDiv = document.querySelector(".story-create");
+const closeModalBtn = document.querySelector(".publish-modal-close");
+const publishNowBtn = document.querySelector(".publish-now-btn");
 
 publishBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  publishModal.classList.remove("hidden");
+  publishModal.classList.add("overlay");
+  storyCreateDiv.classList.add("hidden");
+});
+
+closeModalBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  publishModal.classList.add("hidden");
+  publishModal.classList.remove("overlay");
+  storyCreateDiv.classList.remove("hidden");
+});
+
+publishNowBtn.addEventListener("click", (e) => {
   e.preventDefault();
   formSubmitBtn.click();
 });
