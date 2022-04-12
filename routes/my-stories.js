4b1ba481a9b4b4f3fbdae19a126e4cf480eb3cb1
ethanchen7 <ethan.chen7@gmail.com>
2022-04-12 13:08:45 -0700

    new-story styles, initial route setup

--- routes/my-stories.js
+++ routes/my-stories.js
@@ -5,8 +5,37 @@ const db = require("../db/models");
 const { requireAuth } = require("../auth");
 const { csrfProtection, asyncHandler } = require("./utils");
 
-router.get("/my-stories/new", (req, res) => {
-  res.render("new-story");
+router.get("/my-stories/new", csrfProtection, (req, res) => {
+  res.render("new-story", { crsfToken: req.crsfToken() });
 });
+
+// NEED:
+// requireAuth
+// validators
+// additional inputs from modal (tags)
+// userId when logged in
+router.post(
+  "/my-stories/new",
+  csrfProtection,
+  asyncHandler(async (req, res, next) => {
+    const { title, article } = req.body;
+    // temporary
+    const tagId = 1;
+    const userId = 1;
+    const state = "published";
+
+    const newStory = db.Story.build({
+      title,
+      article,
+      tagId,
+      userId,
+      state,
+    });
+
+    await newStory.save();
+    console.log("New Story created");
+    res.redirect("/feed");
+  })
+);
 
 module.exports = router;
