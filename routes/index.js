914b1e4328dfc68948ce1848c84ab3a2cec76efc
da5idf <davidpforster24@gmail.com>
2022-04-11 20:05:32 -0400

    working on the splash page. need to render trending stories better

--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,24 @@ var router = express.Router();
 
 const db = require("../db/models");
 const { requireAuth } = require("../auth");
-const { csrfProtection } = require("./utils");
+const { csrfProtection, asyncHandler } = require("./utils");
 
 /* GET splash page. */
-router.get("/", requireAuth, csrfProtection, function (req, res, next) {
+router.get("/", requireAuth, csrfProtection, asyncHandler(async (req, res, next) => {
   console.log(req.session.auth);
   if (!req.session.auth) {
     const user = db.User.build();
+    const stories = await db.Story.findAll({ include: [db.User, db.Tag] });
     // change to index after
     res.render("user-register", {
       user,
+      trending: stories,
       csrfToken: req.csrfToken(),
     });
   } else {
     res.render("feed");
   }
-});
+}));
 
 router.get("/login", csrfProtection, function (req, res, next) {
   res.render("user-login", { csrfToken: req.csrfToken() });
