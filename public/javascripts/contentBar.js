6c6404a49355d3aa37cdc378ab18b0d4bcb08da9
ethanchen7 <ethan.chen7@gmail.com>
2022-04-13 16:51:57 -0700

    added tag routes to btns

--- public/javascripts/contentBar.js
+++ public/javascripts/contentBar.js
@@ -1,5 +1,8 @@
 const unlimitedAccessBtn = document.querySelector(".unlimited-access-btn");
 const startWritingBtn = document.getElementById("start-writing-btn");
+const recommendedTopicsTags = document.querySelector(
+  ".recommend-topics-tags"
+).children;
 
 unlimitedAccessBtn.addEventListener("click", (e) => {
   window.location.href =
@@ -9,3 +12,11 @@ unlimitedAccessBtn.addEventListener("click", (e) => {
 startWritingBtn.addEventListener("click", (e) => {
   window.location.href = "/my-stories/new";
 });
+
+for (let i = 0; i < recommendedTopicsTags.length; i++) {
+  let tag = recommendedTopicsTags[i];
+  let tagId = parseInt(tag.id.split("-").at(-1), 10);
+  tag.addEventListener("click", (e) => {
+    window.location.href = `/tags/${tagId}`;
+  });
+}
