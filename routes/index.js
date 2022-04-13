9a30af26ac9c9154d6f79b58815966a2a4d12b2e
ethanchen7 <ethan.chen7@gmail.com>
2022-04-13 15:45:30 -0700

    content bar elements

--- routes/index.js
+++ routes/index.js
@@ -7,51 +7,57 @@ const { csrfProtection, asyncHandler, splashPageQueries } = require("./utils");
 
 /* GET splash page. */
 
-
-router.get("/", csrfProtection, restoreUser, asyncHandler(async (req, res, next) => {
-
-  const queries = await splashPageQueries();
-  const { user, stories, newStories, tags } = queries
-  // console.log("line 15 ********", res.locals.authenticated);
-  if (!res.locals.authenticated) {
-
-    res.render("user-register", {
-      user,
-      newStories,
-      stories,
-      tags,
-      csrfToken: req.csrfToken(),
-    });
-
-  } else {
-    // console.log('line 27 *************', res.locals.user.id)
-    res.render("feed", {
-      user,
-      newStories,
-      stories,
-      tags,
-      csrfToken: req.csrfToken(),
-
-    });
-  }
-}));
+router.get(
+  "/",
+  csrfProtection,
+  restoreUser,
+  asyncHandler(async (req, res, next) => {
+    const queries = await splashPageQueries();
+    const { user, stories, newStories, tags } = queries;
+    // console.log("line 15 ********", res.locals.authenticated);
+    if (!res.locals.authenticated) {
+      res.render("user-register", {
+        user,
+        newStories,
+        stories,
+        tags,
+        csrfToken: req.csrfToken(),
+      });
+    } else {
+      // console.log('line 27 *************', res.locals.user.id)
+      res.render("feed", {
+        user,
+        newStories,
+        stories,
+        tags,
+        csrfToken: req.csrfToken(),
+      });
+    }
+  })
+);
 
 router.get("/login", csrfProtection, function (req, res, next) {
   res.render("user-register", { csrfToken: req.csrfToken() });
 });
 
-router.get("/feed", asyncHandler(async (req, res) => {
-  const queries = await splashPageQueries()
+router.get(
+  "/feed",
+  asyncHandler(async (req, res) => {
+    const queries = await splashPageQueries();
 
-  const { user, stories, tags } = queries
+    const { user, recommendedUsers, stories, newStories, tags } = queries;
 
-
-
-  res.render("feed", {
-    user,
-    stories,
-    tags
-  });
-}));
+    const contentBarStories = newStories.slice(0, 3);
+    const contentBarTags = tags.slice(0, 7);
+    res.render("feed", {
+      user,
+      recommendedUsers,
+      stories,
+      contentBarStories,
+      tags,
+      contentBarTags,
+    });
+  })
+);
 
 module.exports = router;
--- routes/utils.js
+++ routes/utils.js
@@ -1,4 +1,4 @@
-const db = require('../db/models');
+const db = require("../db/models");
 
 const csrf = require("csurf");
 
@@ -9,38 +9,55 @@ const asyncHandler = (handler) => (req, res, next) =>
 
 const splashPageQueries = async () => {
   const user = db.User.build();
+
+  const recommendedUsers = await db.User.findAll({
+    order: [["createdAt", "ASC"]],
+    limit: 3,
+  });
+
   const stories = await db.Story.findAll({
     include: [db.User, db.Tag],
-    order: [['createdAt', 'ASC']],
+    order: [["createdAt", "ASC"]],
   });
 
   const tags = await db.Tag.findAll({
-    order: [['createdAt', 'ASC']],
+    order: [["createdAt", "ASC"]],
     limit: 9,
   });
 
-  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec',]
-  stories.forEach(story => {
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  stories.forEach((story) => {
     const monthIndex = story.updatedAt.getMonth();
     const month = months[monthIndex];
 
-    story.date = `${month} ${story.updatedAt.getDate().toString()}`
+    story.date = `${month} ${story.updatedAt.getDate().toString()}`;
 
     const article = story.article;
-    const articleWords = article.split(' ');
-    const blurb = articleWords.slice(0, 15).join(' ');
+    const articleWords = article.split(" ");
+    const blurb = articleWords.slice(0, 15).join(" ");
     story.blurb = blurb;
-    story.readTime = Math.max(1, Math.floor(articleWords.length / 250))
-
-  })
-  let newStories = []
+    story.readTime = Math.max(1, Math.floor(articleWords.length / 250));
+  });
+  let newStories = [];
   for (let i = 0; i < 6; i++) {
     let story = stories.shift();
     newStories.push(story);
   }
-  return { user, stories, newStories, tags }
-}
+  return { user, recommendedUsers, stories, newStories, tags };
+};
 
 module.exports = {
   csrfProtection,
