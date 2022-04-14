af9d68afc0e29de40924625c69f24142a816daf0
da5idf <davidpforster24@gmail.com>
2022-04-14 12:44:14 -0400

    fixed recommendedUsers bug

--- routes/index.js
+++ routes/index.js
@@ -15,45 +15,45 @@ const {
 
 router.get("/", csrfProtection, restoreUser, asyncHandler(async (req, res, next) => {
 
-    const queries = await splashPageQueries();
-    const { user, stories, newStories, tags } = queries
-    if (!res.locals.authenticated) {
+  const queries = await splashPageQueries();
+  const { user, stories, newStories, tags } = queries
+  if (!res.locals.authenticated) {
 
-        res.render("user-register", {
-            user,
-            newStories,
-            stories,
-            tags,
-            csrfToken: req.csrfToken(),
-        });
+    res.render("user-register", {
+      user,
+      newStories,
+      stories,
+      tags,
+      csrfToken: req.csrfToken(),
+    });
 
-    } else {
-        const queries = await splashPageQueries()
+  } else {
+    const queries = await splashPageQueries()
 
-        const followingQueries = await followingArticles(req, res)
+    const followingQueries = await followingArticles(req, res)
 
-        const { stories, tags } = queries
+    const { recommendedUsers, stories, tags } = queries
 
-        const { followingStories } = followingQueries
+    const { followingStories } = followingQueries
 
-        const userId = req.session.auth.userId
+    const userId = req.session.auth.userId
 
-        const user = await db.User.findByPk(userId)
+    const user = await db.User.findByPk(userId)
 
-        const contentBarStories = newStories.slice(0, 3);
-        const contentBarTags = tags.slice(0, 7);
-        
-        res.render("feed", {
-            user,
-            newStories,
-            recommendedUsers,
-            stories,
-            tags,
-            contentBarStories,
-            contentBarTags,
-            followingStories,
-        });
-    }
+    const contentBarStories = newStories.slice(0, 3);
+    const contentBarTags = tags.slice(0, 7);
+
+    res.render("feed", {
+      user,
+      newStories,
+      recommendedUsers,
+      stories,
+      tags,
+      contentBarStories,
+      contentBarTags,
+      followingStories,
+    });
+  }
 }));
 
 router.get("/login", csrfProtection, function (req, res, next) {
