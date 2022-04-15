4ec5b2bd5aab2853ca3a8430a5428135890a99da
da5idf <davidpforster24@gmail.com>
2022-04-15 18:15:55 -0400

    sequential Ms on splash

--- public/javascripts/disappearingM.js
+++ public/javascripts/disappearingM.js
@@ -1,8 +1,17 @@
 addEventListener('DOMContentLoaded', () => {
 
     const collection = document.querySelectorAll('.disappearingM');
-    let visibleCollection = [];
-    for (let i = 0; i < (17 * 25); i++) {
+
+    let visibleCollection = getVisibleCollection(collection);
+    let toggleCollection = getToggleCollection(visibleCollection);
+
+    // toggleCollection.forEach(el => toggleElement(el));
+    animate(toggleCollection);
+});
+
+function getVisibleCollection(collection) {
+    let visibleCollection = []
+    for (let i = 0; i < collection.length; i++) {
         const rand = Math.random();
         if (rand < .55) {
             collection[i].classList.add('hiddenByColor');
@@ -10,18 +19,46 @@ addEventListener('DOMContentLoaded', () => {
             visibleCollection.push(collection[i]);
         };
     };
+    return visibleCollection;
+}
 
-    visibleCollection.forEach((el) => {
+function getToggleCollection(collection) {
+
+    return collection.filter(el => {
         const rand = Math.random();
         if (rand < .15) {
-            setTimeout(toggleElement(el), 3000);
-            // toggleElement(el);
+            return el;
         }
     });
-});
+}
 
-function toggleElement(element) {
-    setInterval(() => {
-        element.classList.toggle('hiddenByColor');
-    }, 1000);
+// function toggleElement(element) {
+//     setInterval(() => {
+//         element.classList.toggle('hiddenByColor');
+//     }, 1000);
+// };
+
+function toggleElement(element, time) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            element.classList.toggle('hiddenByColor');
+            resolve();
+        }, time)
+    })
+};
+
+async function animate(collection) {
+
+    while (true) {
+        for (let i = 0; i < collection.length; i++) {
+            await toggleElement(collection[i], 100);
+            // if (i === 0) console.log(collection[i].classList);
+        }
+        for (let i = collection.length - 1; i >= 0; i--) {
+            console.log(i);
+            console.log(collection[i])
+            await toggleElement(collection[i], 100);
+            // if (i === collection.length - 1) console.log(collection[i].classList);
+        };
+    };
 };
