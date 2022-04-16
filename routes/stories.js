3917b30f51112c5abffa4c6e657ab467d141b38b
da5idf <davidpforster24@gmail.com>
2022-04-15 21:47:50 -0400

    one small change. add .reverse()

--- routes/stories.js
+++ routes/stories.js
@@ -22,7 +22,7 @@ router.get('/stories/:storyId(\\d+)', restoreUser, requireAuth, asyncHandler(asy
 
 
     const currUser = await db.User.findByPk(userId);
-    const comments = story.Comments;
+    const comments = story.Comments.reverse();
 
     const nonFollowedAccounts = await getRecommended(userId)
 
@@ -41,18 +41,18 @@ router.get('/stories/:storyId(\\d+)', restoreUser, requireAuth, asyncHandler(asy
     const contentBarTags = tags.slice(0, 7);
 
     const allClapsOfStory = await db.StoryClap.findAll({
-        where: {storyId}
+        where: { storyId }
     })
     const allClapsOfStoryCount = allClapsOfStory.length
 
     let follows = false;
     const followCheck = await db.Follow.findAll({
-        where: { 
-          followerId: userId,
-          followingId: story.User.id
-         },
-      })   
-    if(followCheck.length) follows = true; 
+        where: {
+            followerId: userId,
+            followingId: story.User.id
+        },
+    })
+    if (followCheck.length) follows = true;
 
     res.render('single-story', {
         story,
@@ -62,7 +62,7 @@ router.get('/stories/:storyId(\\d+)', restoreUser, requireAuth, asyncHandler(asy
         contentBarTags,
         recommendedUsers,
         nonFollowedAccounts,
-        allClapsOfStoryCount, 
+        allClapsOfStoryCount,
         comments,
         follows,
     });
@@ -77,10 +77,10 @@ router.post('/stories/:id(\\d+)/clap/new', requireAuth, asyncHandler(async (req,
     })
     await newClap.save()
     const allClapsOfStory = await db.StoryClap.findAll({
-        where: {storyId}
+        where: { storyId }
     })
     updatedClapCount = allClapsOfStory.length
-    res.json({updatedClapCount})
+    res.json({ updatedClapCount })
 
 }))
 
