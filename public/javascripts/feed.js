8ba9655fb0ad67d9d1b6d0d048e3b2cb9dea267d
ethanchen7 <ethan.chen7@gmail.com>
2022-04-23 15:08:22 -0700

    delete button styling, delete functionality complete

--- public/javascripts/feed.js
+++ public/javascripts/feed.js
@@ -1,12 +1,4 @@
-const editStoryBtns = document.querySelectorAll('#story-edit-btn')
-const deleteStoryBtns = document.querySelectorAll('#story-delete-btn')
 
-for (let i = 0; i < editStoryBtns.length; i++) {
-    editStoryBtns[i].addEventListener('click', e => {
-        const storyId = editStoryBtns[i].dataset.index.split('-').at(-1)
-        location.href=`/stories/${storyId}/edit`
-    })
-}
 
 window.addEventListener('load', (event) => {
     let recommendedButton = document.getElementById('recommended')
@@ -45,4 +37,70 @@ window.addEventListener('load', (event) => {
     }
 
     followingButton.addEventListener('click', getFollowingStories)
+
+    const editStoryBtns = document.querySelectorAll('#story-edit-btn')
+    const deleteStoryBtns = document.querySelectorAll('#story-delete-btn')
+
+    for (let i = 0; i < editStoryBtns.length; i++) {
+        editStoryBtns[i].addEventListener('click', e => {
+            const storyId = editStoryBtns[i].dataset.index.split('-').at(-1)
+            location.href=`/stories/${storyId}/edit`
+        })
+    }
+
+    for (let i = 0; i < deleteStoryBtns.length; i++) {
+        deleteStoryBtns[i].addEventListener('click', e => {
+            e.stopPropagation()
+            const storyId = deleteStoryBtns[i].dataset.index.split('-').at(-1)
+
+            const deleteModal = document.getElementById('id01')
+            const closeBtn = document.querySelector('.closebtn')
+            const cancelBtn = document.querySelector('.cancelbtn')
+            const sideBar = document.querySelector('.sidebar')
+            const contentBar = document.querySelector('.content-bar')
+            const deleteBtn = document.querySelector('.deletebtn')
+
+            deleteModal.style.display='block';
+
+            closeBtn.addEventListener('click', e => {
+                deleteModal.style.display='none';
+            })
+
+            cancelBtn.addEventListener('click', e => {
+                e.stopPropagation()
+                deleteModal.style.display='none';
+            })
+            sideBar.addEventListener('click', e => {
+                e.stopPropagation()
+                deleteModal.style.display='none';
+            })
+            contentBar.addEventListener('click', e => {
+                e.stopPropagation()
+                deleteModal.style.display='none';
+            })
+            deleteBtn.addEventListener('click', async(e) => {
+                e.stopPropagation()
+                const res = await fetch(`/stories/${storyId}`, {
+                    method: 'DELETE',
+                })
+                if (res.ok) {
+                    const data = await res.json();
+                    deleteModal.style.display='none';
+                } 
+                window.location.href="/"
+            })
+            cancelBtn.addEventListener('mouseover', e => {
+                cancelBtn.style.borderColor = '#000000'
+            })
+            cancelBtn.addEventListener('mouseleave', e => {
+                cancelBtn.style.borderColor = 'rgba(117, 117, 117, 1)'
+            })
+            deleteBtn.addEventListener('mouseover', e => {
+                deleteBtn.style.backgroundColor = 'rgb(150, 22, 22)'
+            })
+            deleteBtn.addEventListener('mouseleave', e => {
+                deleteBtn.style.backgroundColor = 'rgb(201, 74, 74)';
+            })
+        })
+    }
 });
