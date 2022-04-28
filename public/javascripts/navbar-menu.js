4edf3a9d6588679cc4be57641dca0beaec5f06a8
ethanchen7 <ethan.chen7@gmail.com>
2022-04-28 16:50:40 -0700

    fixed navbar menu console error

--- public/javascripts/navbar-menu.js
+++ public/javascripts/navbar-menu.js
@@ -1,50 +1,39 @@
 window.addEventListener("load", (event) => {
+  const icon = document.getElementsByClassName("nav-profile-icon-container")[0];
+  const menu = document.getElementsByClassName(
+    "nav-profile-icon-menu-container"
+  );
 
+  icon.addEventListener("click", (e) => {
+    const currStyle = window
+      .getComputedStyle(menu[0])
+      .getPropertyValue("display");
 
-    const icon = document.getElementsByClassName('nav-profile-icon-container')[0]
-    const menu = document.getElementsByClassName('nav-profile-icon-menu-container');
+    if (currStyle === "none") {
+      menu[0].style.display = "block";
+    } else {
+      menu[0].style.display = "none";
+    }
+  });
 
-    icon.addEventListener('click', (e) => {
-        const currStyle = window.getComputedStyle(menu[0]).getPropertyValue('display')
+  const centerFeed = document.getElementsByClassName("centerHolder")[0];
+  const contentBarFeed = document.querySelectorAll(".content-bar");
 
-        if (currStyle === 'none') {
-            menu[0].style.display = 'block'
-        } else {
-            menu[0].style.display = 'none'
-        }
+  centerFeed.addEventListener("click", () => {
+    menu[0].style.display = "none";
+  });
 
+  contentBarFeed.forEach((el) => {
+    el.addEventListener("click", () => {
+      menu[0].style.display = "none";
     });
+  });
 
-    const centerFeed = document.getElementsByClassName('centerHolder')[0]
-    const contentBarFeed = document.querySelectorAll('.content-bar')
+  const centerUser = document.getElementsByClassName("centerHolderUser")[0];
 
-
-    centerFeed.addEventListener('click', () => {
-
-        menu[0].style.display = 'none'
-    
-
-    })
-
-    contentBarFeed.forEach((el) => {
-        el.addEventListener('click', () => {    
-           
-            menu[0].style.display = 'none'
-            
-        });
-    })
-    
-
-
-    const centerUser = document.getElementsByClassName('centerHolderUser')[0]
-
-    centerUser.addEventListener('click', () => {
-        
-        menu[0].style.display = 'none'
-
+  if (centerUser) {
+    centerUser.addEventListener("click", () => {
+      menu[0].style.display = "none";
     });
-
-
-    
-
-})
+  }
+});
